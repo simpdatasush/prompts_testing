5ec42ea5d82f792d2df3303498d420b7bec73a64
kulsushrut <59820076+simpdatasush@users.noreply.github.com>
2025-07-31 20:05:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1413,4 +1413,3 @@ if __name__ == '__main__':
     # you can use `nest_asyncio.apply()` (install with `pip install nest-asyncio`), but this is
     # generally not recommended for production as it can hide underlying architectural issues.
     app.run(debug=True)
-
